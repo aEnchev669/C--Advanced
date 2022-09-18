7fc4cf784ce39eab0ce33b5d5785ca6c75fe314c
aEnchev669 <aenchev669@abv.bg>
2022-09-18 15:20:31 +0300

    Stacks and Queues - Lab

---
+++ Stacks and Queues/Lab/01. Reverse a String/01. Reverse a String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            int lenght = stack.Count;
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
